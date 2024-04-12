1a4e70d623f9dc5bfc53dbf63a5d9197834839c0
BEnh3N <bhpohlmann@icloud.com>
2024-04-11 21:16:18 -0700

    added title

--- src/main/java/net/benh3n/color_utils/client/gui/screen/TestScreen.java
+++ src/main/java/net/benh3n/color_utils/client/gui/screen/TestScreen.java
@@ -4,6 +4,7 @@ import net.benh3n.color_utils.ColorUtils;
 import net.benh3n.color_utils.client.gui.widget.ColorsListWidget;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
@@ -18,11 +19,17 @@ public class TestScreen extends Screen {
     }
 
     @Override
-    protected void init() {
-        ColorsListWidget colorsListWidget = addDrawableChild(new ColorsListWidget(client, width, height, 0, 20));
+    public void init() {
+        ColorsListWidget colorsListWidget = addDrawableChild(new ColorsListWidget(client, width, height-30, 30, 20));
         for (Map.Entry<Identifier, Integer> entry : ColorUtils.BLOCK_COLORS.entrySet()) {
             colorsListWidget.addEntry(entry.getKey(), entry.getValue());
         }
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        super.render(context, mouseX, mouseY, delta);
+        context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 8, 0xFFFFFF);
     }
 
     @Override
